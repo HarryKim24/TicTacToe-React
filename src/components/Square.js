fd379bdc92e3be8d3af7c8abd2135293f15508f9
HarryKim24 <tl9434@naver.com>
2024-04-13 22:30:13 +0900

    add State

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ import "./Square.css"
 export default class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => {console.log('click')}}>
+      <button className="square" onClick={() => { this.setState({ value: 'X' }) }}>
         {this.props.value}
       </button>
     )
